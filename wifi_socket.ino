4284569e1107c1f436adf7f2b50958b522931306
Yegor Chernyshev <deus.chernyshev@gmail.com>
2020-09-13 00:19:22 +0300

    Сделал отступы 4 пробелами

--- wifi_socket.ino
+++ wifi_socket.ino
@@ -11,69 +11,69 @@ const char *password = "10041997";
 
 void setup()
 {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  WiFi.mode(WIFI_STA); // Отключение режима точки доступа. Варианты: WIFI_AP, WIFI_STA, WIFI_AP_STA
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    WiFi.mode(WIFI_STA); // Отключение режима точки доступа. Варианты: WIFI_AP, WIFI_STA, WIFI_AP_STA
 
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, HIGH);
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, HIGH);
 
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.println("Connecting...");
-  }
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(1000);
+        Serial.println("Connecting...");
+    }
 
-  Serial.print("LOCAL IP OF THE DEVICE IS: ");
-  Serial.print(WiFi.localIP());
-  Serial.print("\n");
+    Serial.print("LOCAL IP OF THE DEVICE IS: ");
+    Serial.print(WiFi.localIP());
+    Serial.print("\n");
 }
 
 void loop()
 {
-  // Проверка статуса подключения
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    HTTPClient http;
-
-    http.begin("http://192.168.0.26:8000/");
-
-    int httpCode = http.GET();
-
-    // Проверка кода ответа HTTP
-    if (httpCode > 0)
+    // Проверка статуса подключения
+    if (WiFi.status() == WL_CONNECTED)
     {
-      // Парсинг JSON с ответа
-      const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
-      DynamicJsonBuffer jsonBuffer(bufferSize);
-      JsonObject &root = jsonBuffer.parseObject(http.getString());
-      
-      // Получаемые параметры из JSON
-      const char *name = root["name"];
-      const char *ip = root["ip"];
-      const bool status = root["status"];
+        HTTPClient http;
 
-      // Вывод данных на монитор порта
-      Serial.print("name:");
-      Serial.println(name);
-      Serial.print("ip:");
-      Serial.println(ip);
-      Serial.print("status:");
-      Serial.println(status);
+        http.begin("http://192.168.0.26:8000/");
 
-      if (status == 1 || status == true)
-      {
-        digitalWrite(RELAY_PIN, LOW);
-      }
-      else if (status == 0 || status == false)
-      {
-        digitalWrite(RELAY_PIN, HIGH);
-      }
+        int httpCode = http.GET();
+
+        // Проверка кода ответа HTTP
+        if (httpCode > 0)
+        {
+            // Парсинг JSON с ответа
+            const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
+            DynamicJsonBuffer jsonBuffer(bufferSize);
+            JsonObject &root = jsonBuffer.parseObject(http.getString());
+
+            // Получаемые параметры из JSON
+            const char *name = root["name"];
+            const char *ip = root["ip"];
+            const bool status = root["status"];
+
+            // Вывод данных на монитор порта
+            Serial.print("name:");
+            Serial.println(name);
+            Serial.print("ip:");
+            Serial.println(ip);
+            Serial.print("status:");
+            Serial.println(status);
+
+            if (status == 1 || status == true)
+            {
+                digitalWrite(RELAY_PIN, LOW);
+            }
+            else if (status == 0 || status == false)
+            {
+                digitalWrite(RELAY_PIN, HIGH);
+            }
+        }
+
+        http.end(); // Закрытие соединения
     }
 
-    http.end(); // Закрытие соединения
-  }
-
-  // Задержка запроса
-  delay(200);
+    // Задержка запроса
+    delay(200);
 }
