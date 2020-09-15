fd6234d5d746cb66b7d427face4dbb49a78d7713
Yegor Chernyshev <deus.chernsyhev@gmail.com>
2020-09-15 12:53:21 +0300

    Настройки вынесены в файл main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Глобольные настройки
+#define RELAY_PIN 4                                 // Номер управляющего пина
+#define SSID "Sumrak"                               // Имя WiFi сети, к которой подключаемся
+#define PASSWORD "10041997"                         // Пароль от WiFi сети 
+#define SERVER_URL "http://192.168.0.26:8000/"      // Адрес backend сервера
+#define HOST_NAME "Smart Socket: MAIN"              // Имя устройства в сети
+#define REQUEST_DELAY 500                           // Задержка отправки запросов на backend (мс)
+
+#endif
--- wifi_socket.ino
+++ wifi_socket.ino
@@ -1,14 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
-
-// Глобольные настройки
-#define RELAY_PIN 4                                 // Номер управляющего пина
-#define SSID "Sumrak"                               // Имя WiFi сети, к которой подключаемся
-#define PASSWORD "10041997"                         // Пароль от WiFi сети 
-#define SERVER_URL "http://192.168.0.26:8000/"      // Адрес backend сервера
-#define HOST_NAME "Smart Socket: MAIN"              // Имя устройства в сети
-#define REQUEST_DELAY 500                           // Задержка отправки запросов на backend (мс)
+#include "main.h"
 
 
 // Функция настройки
